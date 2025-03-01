a466e7ba6a9f9d0b840e3a41eda8350e35c7219d
Miriam Rawlins <miriammrawlins@gmail.com>
2025-03-01 10:56:50 -0700

    dummy data

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
 class MovieApp {
+    popularMovies = ['A New Hope', 'How to Train Your Dragon', 'Megamind'];
 
     constructor() {
         const search = this.getElement('search');
-        search.addEventListener('click', () => {
-            console.log('click');
-        })
-    }
-    popularMovies = [''];
+        search.addEventListener('click', this.searchClicked);
 
+        this.displayPopularMovies();
+    }
 
     searchClicked() {
+        console.log('searchCLicked');
+    }
 
+    displayPopularMovies() {
+        const popularMovieUl = this.getElement('display-movies');
+        this.popularMovies.forEach(movie => {
+            const popularMovieLi = this.createElement('li');
+            popularMovieLi.textContent = movie;
+            popularMovieUl.appendChild(popularMovieLi);
+        })
     }
 
     getElement(id) {
         return document.getElementById(id);
+    }
+    createElement(tag) {
+        return document.createElement(tag);
     }
 }
 
